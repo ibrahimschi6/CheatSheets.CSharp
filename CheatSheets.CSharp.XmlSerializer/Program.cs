56ecd428cd8ab94908b2473187cb94df7cc58fff
ibrahimschi6 <76202393+ibrahimschi6@users.noreply.github.com>
2021-02-19 18:13:33 +0100

    Add Xml Serialization
    Add Xml Serialization to string

---
+++ CheatSheets.CSharp.XmlSerializer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CheatSheets.CSharp.XmlSerializer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SampleClass input = new SampleClass { Id = 1, Name = "First", SubClasses = new List<SampleSubClass>() };
+
+            for (int i = 0; i < 10; i++)
+            {
+                input.SubClasses.Add(new SampleSubClass { Id = 10 + i, Name = "Test" + i });
+            }
+
+            //CALL
+            var testData = SerializeToString(input);
+
+            Console.WriteLine(testData);
+            Console.ReadKey();
+        }
+
+        public static string SerializeToString(object obj)
+        {
+            XmlSerializerFactory serializerFactory = new XmlSerializerFactory();
+            var serializer = serializerFactory.CreateSerializer(obj.GetType());
+
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, obj);
+
+                return writer.ToString();
+            }
+        }
+    }
+        public class SampleClass
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public List<SampleSubClass> SubClasses { get; set; }
+        }
+        public class SampleSubClass
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    
+}
